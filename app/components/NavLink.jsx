4a99a14771d489eec7bf8e7c42988d2df97ef13f
Mohammed Tarek <109961066+Mohammed0tarek@users.noreply.github.com>
2024-10-25 01:04:41 +0300

    Bug link navigation bug

--- app/components/NavLink.jsx
+++ app/components/NavLink.jsx
@@ -1,13 +1,14 @@
-import Link from "next/link"
-const NavLink =({ href , title}) =>{
-  return(
-    <Link 
-    href={href}
-    className='block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white'
+import Link from "next/link";
+const NavLink = ({ href, title }) => {
+  return (
+    <Link
+      target="_blank"
+      href={href}
+      className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
     >
-    {title}
+      {title}
     </Link>
-  )
-}
+  );
+};
 
-export default NavLink
+export default NavLink;
